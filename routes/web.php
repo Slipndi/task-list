66e6548680c23ad70a7cfe4fd9f9349ae66bab36
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-04 22:02:15 +0200

    Création des controllers et routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', CategoryController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('task-lists', TaskListController::class);
