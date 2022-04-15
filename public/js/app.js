bfd8765ff4bd2f4477366b00cbe90f64776b8146
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-15 16:36:16 +0200

    async await addatask

--- public/js/app.js
+++ public/js/app.js
@@ -19579,6 +19579,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   props: ["taskListId"],
   data: function data() {
@@ -19588,16 +19596,38 @@ __webpack_require__.r(__webpack_exports__);
   },
   methods: {
     createTask: function createTask() {
-      if (this.title.length >= 3) {
-        axios.post("tasks", {
-          title: this.title,
-          is_done: false,
-          task_lists_id: this.taskListId
-        });
-        this.$parent.getTasks();
-      }
+      var _this = this;
 
-      this.title = "";
+      return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                if (!(_this.title.length >= 3)) {
+                  _context.next = 4;
+                  break;
+                }
+
+                _context.next = 3;
+                return axios.post("tasks", {
+                  title: _this.title,
+                  is_done: false,
+                  task_lists_id: _this.taskListId
+                });
+
+              case 3:
+                _this.$parent.getTasks();
+
+              case 4:
+                _this.title = "";
+
+              case 5:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }))();
     }
   }
 });
